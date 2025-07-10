a010a93c79607922dba11455cbe4acbe87c6d01f
emilyfranca8 <emily.franca.ribeiro@escola.pr.gov.br>
2025-07-10 15:39:11 -0300

    Aula do dia 10-07

--- main.java
+++ main.java
@@ -1,1 +1,11 @@
+public class main{
+    public static void Main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
 
+        int anoDeLançamento = 2022;
+        System.out.println("Ano de lançamento" + anoDeLançamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+    }
+}
