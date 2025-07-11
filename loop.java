04dcf42042e73d5a06b05d7454fe023e860cab2d
emilyfranca8 <emily.franca.ribeiro@escola.pr.gov.br>
2025-07-11 10:11:55 -0300

    Aula do dia 11-07

---
+++ loop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        double avaliação = 0;
+        double nota = 0;
+
+        for (int i = 0; i < ; i++) {
+            System.out.println("Diga sua avaliação para o filme");
+            nota = leitura.nextDouble();
+            mediaAvaliação += nota;
+        }
+
+        System.out.println("Média de avaliações" + mediaAvaliação/3);
+
+
+    }
+}
